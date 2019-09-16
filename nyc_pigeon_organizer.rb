035157c77d454683407bbecdfd26554e28826cc6
Ruel Tingling <rvtingling@yahoo.com>
2019-09-16 14:42:28 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,35 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+counter = 0 
+  pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+  
+  while pigeon_data [counter] do 
+  puts pigeon_data[counter]
+  counter += 1 
+end 
+
+pigeon_data.each do |color, gender, lives|
+  puts "#{color}; #{gender}; #{lives}""
+end
+  
+  
+  
+  
 end
